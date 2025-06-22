78ca4f5dbb6f6be7a5188181f9cb7353b3f49256
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-06-22 11:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (84.34%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.go
@@ -0,0 +1,15 @@
+func lengthOfLastWord(s string) int {
+    length := 0
+	i := len(s) - 1 
+
+	for i >= 0 && s[i] == ' ' {
+		i--
+	}
+
+	for i >= 0 && s[i] != ' ' {
+		length++
+		i--
+	}
+
+	return length
+}
