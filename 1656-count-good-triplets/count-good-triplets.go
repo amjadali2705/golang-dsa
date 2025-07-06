1fc5b5936b389dedf13f16380f230667edd37410
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-07-06 11:27:06 +0530

    Time: 14 ms (17.65%) | Memory: 4 MB (90.20%) - LeetSync

---
+++ 1656-count-good-triplets/count-good-triplets.go
@@ -0,0 +1,14 @@
+func countGoodTriplets(arr []int, a int, b int, c int) int {
+    count := 0
+
+    for i := 0; i < len(arr); i++ {
+        for j := i+1; j < len(arr); j++ {
+            for k := j+1; k < len(arr); k++ {
+                if math.Abs(float64(arr[i] - arr[j])) <= float64(a) && math.Abs(float64(arr[j] - arr[k])) <= float64(b) && math.Abs(float64(arr[i] - arr[k])) <= float64(c) {
+                    count++
+                }
+            }
+        }
+    }
+    return count
+}
