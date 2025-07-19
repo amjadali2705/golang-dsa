2efc282f5fa0a967d7137e9ac04b2769f97a214c
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-07-19 19:49:38 +0530

    Time: 16 ms (84.78%) | Memory: 8.7 MB (10.87%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.go
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.go
@@ -1,11 +1,32 @@
 func maxProductDifference(nums []int) int {
-    sort.Ints(nums)
+    // Approach 1: Sorting
 
-	n := len(nums)
+    // sort.Ints(nums)
 
-	productMax := nums[n-1] * nums[n-2]
+	// n := len(nums)
+	// productMax := nums[n-1] * nums[n-2]
+	// productMin := nums[0] * nums[1]
+	// return productMax - productMin
 
-	productMin := nums[0] * nums[1]
+    // Approach 2: without sorting
+    max1, max2 := math.MinInt, math.MinInt
+	min1, min2 := math.MaxInt, math.MaxInt
 
-	return productMax - productMin
+	for _, num := range nums {
+		if num >= max1 { 
+			max2 = max1
+			max1 = num
+		} else if num > max2 {
+			max2 = num
+		}
+
+		if num <= min1 { 
+			min2 = min1
+			min1 = num
+		} else if num < min2 {
+			min2 = num
+		}
+	}
+
+	return (max1 * max2) - (min1 * min2)
 }
