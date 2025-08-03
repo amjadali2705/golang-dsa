d559a9ca88c1ff38ab980f9a456ab99edab6f7a0
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-08-03 12:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 4.7 MB (1.85%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.go
@@ -0,0 +1,9 @@
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+    count := 0
+    for _, hour := range hours {
+        if hour >= target {
+            count++
+        }
+    }
+    return count
+}
