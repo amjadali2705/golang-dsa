4d0b2061151449f487734c6e434adb8bc8668408
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-08-16 18:04:52 +0530

    Time: 22 ms (62.85%) | Memory: 7.7 MB (97.50%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -0,0 +1,15 @@
+func firstUniqChar(s string) int {
+    charCount := make(map[rune]int)
+
+	for _, char := range s {
+		charCount[char]++
+	}
+
+	for i, char := range s {
+		if charCount[char] == 1 {
+			return i
+		}
+	}
+
+	return -1
+}
