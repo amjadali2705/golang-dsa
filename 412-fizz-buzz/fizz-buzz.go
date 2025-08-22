0383d753b9af78de2f3fbdb27510a31367a913a8
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-08-22 14:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (11.06%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.go
@@ -0,0 +1,18 @@
+func fizzBuzz(n int) []string {
+    answer := []string{}
+
+    for i := 1; i <= n; i++ {
+        if i % 3 == 0 && i % 5 == 0 {
+            answer = append(answer, "FizzBuzz")
+        } else if i % 3 == 0 {
+            answer = append(answer, "Fizz")
+        } else if i % 5 == 0 {
+           answer = append(answer, "Buzz")
+        } else {
+            answer = append(answer, strconv.Itoa(i))
+        }
+
+    }
+
+    return answer
+}
