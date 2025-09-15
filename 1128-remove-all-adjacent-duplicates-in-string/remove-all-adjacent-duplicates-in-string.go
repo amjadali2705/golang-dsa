a691aaccf08f5e3b91d1f965e38489fcc2a12003
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-09-15 21:39:28 +0530

    Time: 2 ms (65.32%) | Memory: 8.7 MB (52.70%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.go
@@ -0,0 +1,13 @@
+func removeDuplicates(s string) string {
+    stack := []rune{}
+
+    for _, char := range s {
+        if len(stack) > 0 && stack[len(stack)-1] == char {
+            stack = stack[:len(stack)-1]
+        } else {
+            stack = append(stack, char)
+        }
+    }
+
+    return string(stack)
+}
