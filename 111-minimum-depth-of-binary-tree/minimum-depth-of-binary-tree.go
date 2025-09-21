e9d162d24ebc132684e4363cfc7772ac37d39bd5
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-09-21 11:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (99.38%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    if root.Left == nil && root.Right == nil {
+        return 1
+    }
+
+    if root.Left == nil {
+        return 1 + minDepth(root.Right)
+    }
+
+    if root.Right == nil {
+        return 1 + minDepth(root.Left)
+    }
+
+    leftDepth := minDepth(root.Left)
+    rightDepth := minDepth(root.Right)
+
+    return 1 + int(math.Min(float64(leftDepth), float64(rightDepth)))
+}
+
