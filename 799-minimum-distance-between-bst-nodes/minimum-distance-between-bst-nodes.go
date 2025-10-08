694443de2527a10c9654d4945d63c87c133e2fa4
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-10-08 21:33:26 +0530

    Time: 0 ms (100.00%) | Memory: 4.4 MB (36.17%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+    minDiff := math.MaxInt64
+    prevVal := -1
+
+    inOrderTraversal(root, &minDiff, &prevVal)
+
+    return minDiff
+}
+
+func inOrderTraversal(node *TreeNode, minDiff *int, prevVal *int) {
+    if node == nil {
+        return
+    }
+
+    inOrderTraversal(node.Left, minDiff, prevVal)
+
+    if *prevVal != -1 {
+        diff := int(math.Abs(float64(node.Val - *prevVal)))
+        if diff < *minDiff {
+            *minDiff = diff
+        }
+    }
+
+    *prevVal = node.Val
+
+    inOrderTraversal(node.Right, minDiff, prevVal)
+}
