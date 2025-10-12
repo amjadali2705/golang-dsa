7ce3ba16ae1d25d11456733deff728c3be05331d
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2025-10-12 12:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 4.3 MB (56.41%) - LeetSync

---
+++ 933-increasing-order-search-tree/increasing-order-search-tree.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func increasingBST(root *TreeNode) *TreeNode {
+    return dfs(root, nil)
+}
+
+func dfs(root, tail *TreeNode) *TreeNode {
+	if root == nil {
+		return tail
+	}
+	
+	res := dfs(root.Left, root)
+	root.Left = nil
+	root.Right = dfs(root.Right, tail)
+	
+	return res
+}
