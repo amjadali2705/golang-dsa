dc0ad311513ef1ed7d2cac474e982c8e52ad7365
Md Amjad Ali <91709604+amjadali2705@users.noreply.github.com>
2026-03-03 13:53:27 +0530

    Time: 2 ms (77.75%) | Memory: 19.9 MB (85.68%) - LeetSync

---
+++ 979-di-string-match/di-string-match.py
@@ -0,0 +1,16 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        n = len(s)
+        low, high = 0, n
+        perm = []
+        
+        for char in s:
+            if char == 'I':
+                perm.append(low)
+                low += 1
+            else:
+                perm.append(high)
+                high -= 1
+                
+        perm.append(low)
+        return perm
